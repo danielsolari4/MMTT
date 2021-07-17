22f5f3ec3986f75259d97964f20aa4f0ed2ebf3b
danielsolari4 <danielsolari4@gmail.com>
2021-07-17 02:34:09 -0300

    .

---
+++ mmTt/Models/Producto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mmTt.Models
+{
+
+    public class SizesPaths
+    {
+        public string Size1Path { get; set; }
+        public string Size2Path { get; set; }
+        public string Size3Path { get; set; }
+        public string Size4Path { get; set; }
+        public string Size5Path { get; set; }
+    }
+
+    public class Producto
+    {
+        public string Description { get; set; }
+        public List<string> Keywords { get; set; }
+        public string MediaUrl { get; set; }
+        public string Title { get; set; }
+        public int Id { get; set; }
+        public List<string> Categories_Name { get; set; }
+        public SizesPaths SizesPaths { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+
+
+}
---
+++ mmTt/Models/Usuario.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mmTt.Models
+{
+    public class Usuario
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Cellphone { get; set; }
+
+        public string Country { get; set; }
+
+        public string State { get; set; }
+
+        public string City { get; set; }
+
+        public string ZipCode { get; set; }
+
+        [Required]
+        public string LanguageId { get; set; }
+
+        [Required]
+        public string TimezoneId { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        //[CustomPassword]
+        //[MinLength(8, ErrorMessage = "Your password must be at least 8 characters")]
+        //[RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z\d]).{4,}$", ErrorMessage = "Your password must not contain any special characters, symbols or spaces")]
+
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Fields \"Password\" and \"Confirm Password\" must be equal")]
+        public string ConfirmPassword { get; set; }
+
+        public string ProfileImagePath { get; set; }
+    }
+}
